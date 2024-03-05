fb08468927b2316d9305230bb5d05c6147223ce5
hawi-claire <7hawi7@gmail.com>
2024-03-05 14:42:32 -0800

    feat: __init__ method for models directory

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/isr/bin/python3
+"""__init__ method for models directory"""
+
+from models.engine.file_storage import FileStorage
+
+"""Create a single instance of the FileStorage class for the entire application"""
+storage = FileStorage()
+
+"""Load previously saved objects from the storage file (if it exists)"""
+storage.reload()
+
