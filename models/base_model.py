9f0dce98f3dfb1ec05022a20fa1be946d3eb66c3
George Ikigu <ikigu.geo@gmail.com>
2024-03-06 10:17:50 +0300

    fix: remove undefined names

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Define the BaseModel class."""
-import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -8,32 +7,19 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initialize a new BaseModel.
 
         Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            self: BaseModel instance
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                elif k == "__class__":
-                    pass
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
