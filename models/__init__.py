b6289faa5d9ba6804a6736ce84371e01b7cae0c8
George Ikigu <ikigu.geo@gmail.com>
2024-03-07 16:58:00 +0300

    style: remove unnecessary white space

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,8 @@
 
 from models.engine.file_storage import FileStorage
 
- """Create a single instance of the FileStorage class for the entire application"""
- storage = FileStorage()
+"""Create a single instance of the FileStorage class for the entire application"""
+storage = FileStorage()
 
- """Load previously saved objects from File.storage"""
- storage.reload()
+"""Load previously saved objects from the storage file (if it exists)"""
+storage.reload()
