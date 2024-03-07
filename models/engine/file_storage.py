6d74b7b5d526e16ef2b6b8fee6083d5c2169a542
hawi-claire <7hawi7@gmail.com>
2024-03-06 23:07:48 -0800

    fix: store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,11 +3,6 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -47,3 +42,4 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
+        
