fc7998c4d5c3b9cf0e506560c26bae785d7ed78f
George Ikigu <ikigu.geo@gmail.com>
2024-03-07 16:53:57 +0300

    refactor: reorganize test files

--- tests/test_models/test_engine/test_file_storage.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-"""Tests the storage engine"""
-
-import unittest
-from models.engine.file_storage import FileStorage
---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+"""Tests the storage engine"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models import storage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Tests the FileStorage class"""
+
+    # Test public instance methods
+    def tests_all_method(self):
+        """Tests all method"""
+
+        base_model_instance = BaseModel()
+        base_model_instance.name = "new_object"
+        base_model_instance.save()
+
+        all_objects = storage.all()
+
+        self.assertEqual(
+            all_objects[base_model_instance.id], base_model_instance.to_dict())
