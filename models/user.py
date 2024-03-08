45f2b7f1e6df48aff1a5c6c0816c1a67bbe959bc
George Ikigu <ikigu.geo@gmail.com>
2024-03-08 08:11:39 +0300

    fix (reqs): make attributes class attributes
    Remove attributes from init method and move them to class to meet requirements

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Implements the User class"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """The user class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self):
+        super().__init__()
