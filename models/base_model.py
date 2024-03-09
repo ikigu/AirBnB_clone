61861e370142bfe1d73824b99b8b1af525a7a0e0
George Ikigu <ikigu.geo@gmail.com>
2024-03-09 14:12:38 +0300

    clean: remove pcode

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,17 @@ class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """
+        Initialize a new BaseModel. When kwargs are not passed,
+        a new object is created. Kwargs is passed when the
+        object is getting recreated from file storage.
 
         Args:
             self: BaseModel instance
-            *args (unused): positional arguments
+            *args: unused positional arguments
             **kwargs: keyword arguments
         """
 
-        # Kwargs not None means you're creating objects from a file
         if kwargs is not None and kwargs != {}:
             for k, v in kwargs.items():
                 if k == 'created_at' or k == 'updated_at':
@@ -26,30 +28,36 @@ class BaseModel:
                 elif k != '__class__':
                     self.__dict__[k] = v
         else:
-            # This means you're creating a new object
+
             self.id = str(uuid4())
             self.created_at = datetime.today()
             self.updated_at = self.created_at
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """
+        Updates updated_at attribute with the current
+        datetime and writes the instance to file.
+        """
         self.updated_at = datetime.today()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
         """
-        rdict = copy.deepcopy(self.__dict__)
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        Creates an extended dict representation of a BaseModel instance
+        by adding a key/value pair -> __class__: <class_name>
+
+        Args:
+            self: represents the object instance
+        """
+        extended_dict = self.__dict__.copy()
+        extended_dict["created_at"] = self.created_at.isoformat()
+        extended_dict["updated_at"] = self.updated_at.isoformat()
+        extended_dict["__class__"] = self.__class__.__name__
+
+        return extended_dict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the string representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
