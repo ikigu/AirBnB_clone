9e35d08fcab0f3f0a297bc0b8101005f26eebc33
George Ikigu <ikigu.geo@gmail.com>
2024-03-09 14:12:25 +0300

    fix: call basemodel init function

--- models/place.py
+++ models/place.py
@@ -4,7 +4,11 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Represent a place."""
+    """
+    Represent a Place. BaseModel creates a date and time
+    for when the object was instantiated and assigns an id
+    to the object.
+    """
 
     city_id = ""
     user_id = ""
@@ -16,4 +20,7 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = []
+    amenity_ids: list = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
